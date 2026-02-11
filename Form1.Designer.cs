ec7b8f1a49f5db4d7793cb78a0d791ce90ffec90
Muhamed Mehdi Çejku <116339974+MuhamedMehdiCejku@users.noreply.github.com>
2026-02-11 22:53:26 +0100

    Data Encryption Standard (DES)
    Example of DES Algorithm (through Forms).
    
    Key: ________
    Plaintext: ____________
    Ciphertext: ___________
    
    Option 1: Encrypt ||| Option 2: Decrypt

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.input = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Plaintext:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 250);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ciphertext";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Key:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(229, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 50);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Kripto";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(539, 327);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 50);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Dekripto";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(229, 148);
+            this.input.Multiline = true;
+            this.input.Name = "input";
+            this.input.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.input.Size = new System.Drawing.Size(429, 55);
+            this.input.TabIndex = 8;
+            this.input.TextChanged += new System.EventHandler(this.input_TextChanged);
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(229, 229);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.output.Size = new System.Drawing.Size(429, 55);
+            this.output.TabIndex = 9;
+            this.output.TextChanged += new System.EventHandler(this.output_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(229, 73);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(238, 55);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Algoritmi_DES";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+
+        public string EncryptText(string plainText, string key)
+        {
+            des.Key = Encoding.UTF8.GetBytes(key);
+            des.IV = Encoding.UTF8.GetBytes(key);
+            byte[] inputBytes = Encoding.UTF8.GetBytes(plainText);
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(inputBytes, 0, inputBytes.Length);
+            cs.FlushFinalBlock();
+            return Convert.ToBase64String(ms.ToArray());
+        }
+
+        public string DecryptText(string cipherText, string key)
+        {
+            des.Key = Encoding.UTF8.GetBytes(key);
+            des.IV = Encoding.UTF8.GetBytes(key);
+            byte[] inputBytes = Convert.FromBase64String(cipherText);
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write);
+            cs.Write(inputBytes, 0, inputBytes.Length);
+            cs.FlushFinalBlock();
+            return Encoding.UTF8.GetString(ms.ToArray());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string key = textBox1.Text;
+            if (key.Length != 8)
+            {
+                MessageBox.Show("Key must be exactly 8 characters.");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(input.Text))
+            {
+                MessageBox.Show("Enter plaintext to encrypt.");
+                return;
+            }
+
+            output.Text = EncryptText(input.Text, key);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string key = textBox1.Text;
+            if (key.Length != 8)
+            {
+                MessageBox.Show("Key must be exactly 8 characters.");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(output.Text))
+            {
+                MessageBox.Show("Enter ciphertext to decrypt.");
+                return;
+            }
+
+            input.Text = DecryptText(output.Text, key);
+        }
+
+        private void input_TextChanged(object sender, EventArgs e) { }
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        private void output_TextChanged(object sender, EventArgs e) { }
+    }
+}
